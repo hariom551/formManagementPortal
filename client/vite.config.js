376b0ce45f010584987b792017dfa0508e074626
hariom nathani <hariomnathani551@gmail.com>
2024-07-12 00:26:38 +0530

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Allows access from other devices on the network
-    port: 5173,       // Ensures the server runs on port 5173
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'https://formmanagementportal-server.onrender.com/',  // The backend server URL
-        changeOrigin: true,  // Needed for virtual hosted sites
-        // rewrite: (path) => path.replace(/^\/api/, '') // Uncomment if you need to remove the /api prefix
+        target: 'https://formmanagementportal-server.onrender.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   }
