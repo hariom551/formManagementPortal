b8f227c36d69002868c373d2f0edd971cb5ccccb
hariom nathani <hariomnathani551@gmail.com>
2024-07-12 13:09:37 +0530

    sq

--- server/src/app.js
+++ server/src/app.js
@@ -12,10 +12,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-    origin: "https://form-management-portal.vercel.app",
+    origin: ["https://form-management-portal.vercel.app"],
     methods: ["GET", "POST", "PUT","DELETE", "PATCH", "HEAD"],
     credentials: true
 }));
+
+
+// app.use(
+//     cors({
+//       origin: ["http://localhost:5173/"],
+//       methods: ["GET", "POST", "PUT", "DELETE"],
+//       credentials: true,
+//     })
+//   );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
