e996399c26b7b7da79a087ee69afedf8835d0d56
hariom nathani <hariomnathani551@gmail.com>
2024-07-12 16:15:37 +0530

    ok

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,16 +6,14 @@ console.log('Vite proxy configuration loaded');
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
     proxy: {
       '/api': {
         target: 'https://form-management-portal-server.vercel.app/',
         changeOrigin: true,
-        rewrite: (path) => {
-          console.log('Rewriting path:', path);
-          return path.replace(/^\/api/, '');
-        },
+        // rewrite: (path) => {
+        //   console.log('Rewriting path:', path);
+        //   return path.replace(/^\/api/, '');
+        // },
       },
     },
   },
