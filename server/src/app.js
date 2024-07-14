2d8d10599d5df614e7e6b0d2c50bc3f1becad7d7
hariom nathani <hariomnathani551@gmail.com>
2024-07-14 13:04:34 +0530

    testing

--- server/src/app.js
+++ server/src/app.js
@@ -12,20 +12,31 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // CORS configuration
-const corsOptions = {
-    // CORS_ORIGIN=,http://localhost:3000
+// const corsOptions = {
+//     // CORS_ORIGIN=,http://localhost:3000
 
-    origin: ["https://form-management-portal-whgo.vercel.app"],
-    // origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
-    // allowedHeaders: ["Content-Type"],
-    credentials: true
-};
+//     origin: ["https://form-management-portal-whgo.vercel.app"],
+//     // origin: process.env.CORS_ORIGIN,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+//     // allowedHeaders: ["Content-Type"],
+//     credentials: true
+// };
 
-app.use(cors(corsOptions));
+app.use(cors(
+    {
+      
+    
+        origin: ["https://form-management-portal-whgo.vercel.app"],
+        // origin: process.env.CORS_ORIGIN,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+        allowedHeaders: ["Content-Type"],
+        credentials: true
+    }
+
+));
 
 // Handle preflight OPTIONS requests
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
